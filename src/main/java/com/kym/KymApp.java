1f4e53544d59c3ce8ebb06af85bf2c354a9518e1
Rohit Ahuja <rohit23ahuja@gmail.com>
2024-11-10 21:47:30 +0530

    Executable maven project

---
+++ src/main/java/com/kym/KymApp.java
@@ -0,0 +1,13 @@
+package com.kym;
+
+/**
+ * Hello world!
+ *
+ */
+public class KymApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
