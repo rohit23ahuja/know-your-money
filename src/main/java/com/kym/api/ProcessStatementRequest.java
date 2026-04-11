f3e9eba62b56cd45e7228c8642fa6ea8e91c95e3
Rohit Ahuja <rohit23ahuja@gmail.com>
2026-04-11 14:12:43 +0530

    refactoring

--- src/main/java/com/kym/api/ProcessStatementRequest.java
+++
@@ -1,4 +0,0 @@
-package com.kym.api;
-
-public record ProcessStatementRequest(String accountName, String accountNumber, String statementType, String bankCode) {
-}
--- src/main/java/com/kym/controller/StatementProcessingController.java
+++ src/main/java/com/kym/controller/StatementProcessingController.java
@@ -1,12 +1,10 @@
 package com.kym.controller;
 
-import com.kym.api.ProcessStatementRequest;
 import com.kym.api.ProcessStatementResponse;
 import com.kym.service.StatementProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
