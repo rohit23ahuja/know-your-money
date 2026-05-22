5c1d757fe9a38472d95e6bd2d69dc443cd08e216
Rohit Ahuja <rohit23ahuja@gmail.com>
2026-05-22 21:26:19 +0530

    categorization updates

--- src/main/java/com/kym/service/CreditCardTransactionCategorizationService.java
+++ src/main/java/com/kym/service/CreditCardTransactionCategorizationService.java
@@ -1,7 +1,7 @@
 package com.kym.service;
 
-import com.kym.entity.CreditCardTransaction;
 import com.kym.dto.CreditCardTransactionCategorization;
+import com.kym.entity.CreditCardTransaction;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -24,9 +24,15 @@ public class CreditCardTransactionCategorizationService {
                     description.contains("BLINKIT") ||
                     description.contains("RSP*INSTAMART BANGALORE ") ||
                     description.contains("INSTAMART")) {
-                transactionCategorization.append("Grocery");
+                transactionCategorization.append("OnlineGrocery");
                 transactionCategorization.append(";");
             }
+
+            if (description.contains("SHOP EXPRESS 24 X7 PRI FARIDABAD")) {
+                transactionCategorization.append("OfflineGrocery");
+                transactionCategorization.append(";");
+            }
+
             if (description.contains("FASTAG") ||
                     description.contains("HDFC BANK FASTAG MUMBAI ")) {
                 transactionCategorization.append("Travel");
@@ -39,7 +45,9 @@ public class CreditCardTransactionCategorizationService {
                     description.contains("PETRO") ||
                     description.contains("PETROL") ||
                     description.contains("ANAND FILLING STATION DELHI ") ||
-                    description.contains("SHIV SHAKTI PETRO GURGAON ")) {
+                    description.contains("SHIV SHAKTI PETRO GURGAON ") ||
+                    description.contains("SHREE BALAJI FUELS PALWAL") ||
+                    description.contains("FUEL")) {
                 transactionCategorization.append("Fuel");
                 transactionCategorization.append(";");
             }
@@ -49,6 +57,14 @@ public class CreditCardTransactionCategorizationService {
                 transactionCategorization.append(";");
             }
 
+            if (description.contains("Payu*Swiggy Food Bangalore ") ||
+                    description.contains("WWW SWIGGY IN BANGALORE") ||
+                    description.contains("Swiggy") ||
+                    description.contains("BUNDL TECHNOLOGIES BENGALURU ") ||
+                    description.contains("BUNDL TECHNOLOGIES")) {
+                transactionCategorization.append("Food");
+                transactionCategorization.append(";");
+            }
             if (description.contains("CAFE") ||
                     description.contains("cafe") ||
                     description.contains("restaurant") ||
@@ -57,17 +73,18 @@ public class CreditCardTransactionCategorizationService {
                     description.contains("INDRA COFFEE ROASTER P RGURUGRAM ") ||
                     description.contains("KAMATHS NATURAL RETAIL NEW DELHI ") ||
                     description.contains("ICE CREAM COMPANY Gurgaon ") ||
-                    description.contains("Payu*Swiggy Food Bangalore ") ||
-                    description.contains("Swiggy") ||
-                    description.contains("BUNDL TECHNOLOGIES BENGALURU ") ||
-                    description.contains("BUNDL TECHNOLOGIES") ||
                     description.contains("BREW VILLA UDAIPUR ") ||
                     description.contains("RESTAURANT AMBRAI A UN UDAIPUR ") ||
                     description.contains("PPSL*Jubilant Foodwork Noida ") ||
-                    description.contains("Jubilant")) {
+                    description.contains("Jubilant") ||
+                    description.contains("Arcelia BANGALORE") ||
+                    description.contains("BBIEGE 15 BANGALORE")) {
                 transactionCategorization.append("Dineout");
                 transactionCategorization.append(";");
-                transactionCategorization.append("Food");
+            }
+
+            if (description.contains("URBANCLAP TECHNOLOGIES GURGAON")) {
+                transactionCategorization.append("HouseMaintenance");
                 transactionCategorization.append(";");
             }
 
@@ -97,9 +114,9 @@ public class CreditCardTransactionCategorizationService {
 
             if ((description.contains("BHARTI AIRTEL LTD GURGAON ") ||
                     description.contains("BHARTI AIRTEL")) &&
-                            (creditCardTransaction.getAmt() != null &&
-                                    creditCardTransaction.getAmt().compareTo(new BigDecimal("1169")) == 1 &&
-                                    creditCardTransaction.getAmt().compareTo(new BigDecimal("1181")) == -1)) {
+                    (creditCardTransaction.getAmt() != null &&
+                            creditCardTransaction.getAmt().compareTo(new BigDecimal("1169")) == 1 &&
+                            creditCardTransaction.getAmt().compareTo(new BigDecimal("1181")) == -1)) {
                 transactionCategorization.append("Monthly");
                 transactionCategorization.append(";");
                 transactionCategorization.append("Internet");
@@ -107,37 +124,33 @@ public class CreditCardTransactionCategorizationService {
             }
 
             if (description.contains("HOUSE OF DIAGNOSTICS H ENEW DELHI ") ||
-                    description.contains("HOUSE OF DIAGNOSTICS")) {
+                    description.contains("HOUSE OF DIAGNOSTICS") ||
+                    description.contains("IMPERIAL SMILES DENTA GURGAON")) {
                 transactionCategorization.append("Medical");
                 transactionCategorization.append(";");
-                transactionCategorization.append("Health");
+            }
+
+            if (description.contains("Chandigarh") ||
+                    description.contains("MMT HOTEL VIA SMARTBU BANGALORE") ||
+                    description.contains("SHIMLA") ||
+                    description.contains("UDAIPUR")) {
+                transactionCategorization.append("Vacation");
                 transactionCategorization.append(";");
             }
 
-            if (description.contains("UDAIPUR")) {
-                transactionCategorization.append("Travel");
+            if (description.contains("CALCUTTA SAREE SELECTI OGURGAON") ||
+                    description.contains("MEENA BAZAAR GURGAON")) {
+                transactionCategorization.append("OfflineShopping");
                 transactionCategorization.append(";");
             }
 
-            if (description.contains("SHIMLA")) {
-                transactionCategorization.append("Travel");
-                transactionCategorization.append(";");
-            }
-
-            if (description.contains("Chandigarh")) {
-                transactionCategorization.append("Travel");
-                transactionCategorization.append(";");
-            }
-
-            if (description.contains("AMAZON PAY INDIA PRIVA www.amazon.i ") ||
-                    description.contains("www.amazon.i") ||
-                    description.contains("AMAZON PAY INDIA PRIVA Bangalore ") ||
-                    description.contains("AMAZON PAY INDIA") ||
-                    description.contains("NYKAA E RETAIL LIMI BANGALORE ") ||
-                    description.contains("MYNTRA DESIGNS PRIVATE BANGALORE ") ||
-                    description.contains("MYNTRA") ||
-                    description.contains("Myntra Designs Pvt Ltd BANGALORE ") ||
-                    description.contains("GYFTR VIA SMARTBUY NEW DELHI")) {
+            if (description.contains("Myntra Designs Pvt Ltd BANGALORE") ||
+                    description.contains("GYFTR VIA SMARTBUY NEW DELHI") ||
+                    description.contains("AMAZON PAY INDIA PRIVA Bangalore") ||
+                    description.contains("Cashfree*Myntra BANGALORE") ||
+                    description.contains("MYNTRA DESIGNS BANGALORE") ||
+                    description.contains("WWW AMAZON IN GURGAON") ||
+                    description.contains("AMAZON PAY INDIA PRIVA www.amazon.i")) {
                 transactionCategorization.append("OnlineShopping");
                 transactionCategorization.append(";");
             }
@@ -146,6 +159,22 @@ public class CreditCardTransactionCategorizationService {
                 transactionCategorization.append("InternationalTransaction");
                 transactionCategorization.append(";");
             }
+
+            if (description.contains("DISTRICT MOVIE TICKE NEW DELHI") ||
+                    description.contains("Movie") ||
+                    description.contains("PVR") ||
+                    description.contains("INOX") ||
+                    description.contains("RSP*DISTRICT MOVIE TI GURUGRAM")) {
+                transactionCategorization.append("Movie");
+                transactionCategorization.append(";");
+            }
+
+            if (description.contains("UBER INDIA SYSTE PVT NOIDA")) {
+                transactionCategorization.append("Cab");
+                transactionCategorization.append(";");
+            }
+
+
             creditCardTransactionCategorizations.add(new CreditCardTransactionCategorization(
                     creditCardTransaction.getStatementFileId(),
                     creditCardTransaction.getId(),
